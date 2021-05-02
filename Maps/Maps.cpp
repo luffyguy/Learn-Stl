725c0b069f7c843576c8a6b2843d10ccf8722877
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-02 14:53:11 +0530

    diff functions like clear, find, erase

--- Maps/Maps.cpp
+++ Maps/Maps.cpp
@@ -1,19 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printMap(map<int, string> m)
+{
+
+    cout << "Size : " << m.size() << endl;
+    for (auto &pr : m) //o(log(n))
+    {
+        cout << pr.first << " " << pr.second << endl;
+    }
+}
+
 int main()
 {
 
     map<int, string> m;
-    m[1] = "abcd";
+    m[1] = "abcd"; //0(log(n))
     m[3] = "ijkl";
     m[2] = "efgh";
     m.insert({{4, "mnop"}, {5, "qrst"}});
 
-    for (auto &pr : m)
+    auto it = m.find(6);
+    if (it != m.end())
     {
-        cout << pr.first << " " << pr.second << endl;
+        m.erase(3); //0(log(n))
     }
+    m.clear();//clears the map
+    printMap(m);
 
     return 0;
 }
