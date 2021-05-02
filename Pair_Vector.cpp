2d76671dbd684b8020b298046509052b85260720
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-02 09:57:05 +0530

    first commit

---
+++ Pair_Vector.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<int> v)
+{
+    cout << "Size : " << v.size() << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    vector<int> v;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        printVec(v);
+        v.push_back(x);
+    }
+    printVec(v);
+
+    return 0;
+}
