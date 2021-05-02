c3d81b48f68498461c6202ccefca35c76dc1c36d
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-02 13:02:27 +0530

    minor changes nd Iterators usage

---
+++ Iterators/Iterator.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    vector<int> v = {2, 3, 4, 5, 6, 7, 8};
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    vector<int>::iterator it = v.begin();
+    for (it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << endl;
+    }
+    return 0;
+}
