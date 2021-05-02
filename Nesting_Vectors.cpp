f14af4caa2c6c679297e787dd5a3f4a0654072d6
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-02 11:21:52 +0530

    Vector of array

--- Nesting_Vectors.cpp
+++ Nesting_Vectors.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+//vector of pairs
+
+/* #include <bits/stdc++.h>
 using namespace std;
 
 void printVec(vector<pair<int, int>> v)
@@ -26,6 +28,48 @@ int main()
         v.push_back({x, y});
     }
     printVec(v);
+
+    return 0;
+} */
+
+//vector of arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<int> v)
+{
+    cout << "Size : " << v.size() << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> v[n]; //n number of seeprate vectors will be formed
+
+    for (int i = 0; i < n; i++)
+    {
+        int m;
+        cin >> m; //size of eah vector
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            cin >> x;
+            v[i].push_back(x);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printVec(v[i]); //each vector goes in seperately to get printed
+    }
 
     return 0;
 }
