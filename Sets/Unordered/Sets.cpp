75fb76d5095bdac15a2e280c5159553e5d9c0f5d
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-04 17:55:05 +0530

    unordered sets

---
+++ Sets/Unordered/Sets.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    unordered_set<int> s;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    for (auto value : s)
+    {
+        cout << value << endl;
+    }
+
+    return 0;
+}
